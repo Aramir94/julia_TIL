accc03f37cf3152a6c3ef5a48d3618e3fa83fd27
Aramir-A1DATAINT <jeonghyunsuk777@gmail.com>
2022-08-04 14:13:14 +0900

    condition

---
+++ juliaAcademy/IntroductionAcademy/04_condition.jl
@@ -0,0 +1,21 @@
+if (N % 3 == 0) && (N % 5 == 0) # `&&` means "AND"; % computes the remainder after division
+    println("FizzBuzz")
+elseif N % 3 == 0
+    println("Fizz")
+elseif N % 5 == 0
+    println("Buzz")
+else
+    println(N)
+end
+
+
+x = 10
+y = 30
+
+if x > y
+    x
+else
+    y
+end
+
+(x > y) ? x : y # true 면 x, false면 y 
